089304d30fdfbafa98a4b55997e9ffe521a78a67
Joao Branquinho <joaobranquinho@ua.pt>
2018-10-31 15:22:45 +0000

    clean up

--- assignment2/bytes.cpp
+++
@@ -1,9 +0,0 @@
-	int b = (int) floor(log2(n))
-	int k = b, t = n;
-	while (t > 1) { k++;  t >>= 1; }
-
-	// Set u to the number of unused codewords = 2^(k+1) - n.
-	int u = (1 << k+1) - n;
-
-	if (x < u)  return Binary(x, k); 
-        else  return Binary(x+u, k+1));
