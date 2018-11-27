03b7c4cb05d9221263a8058c9333b998dc3bbadd
Luís Silva <lfssilva@ua.pt>
2018-11-27 23:15:48 +0000

    Implement yuv420 parsing on the player

--- assignment3/src/Player.cpp
+++ assignment3/src/Player.cpp
@@ -20,7 +20,8 @@ ReaderWriterQueue<Mat> q(100);
  *
  * Returns a map with the values parsed
 */
-map<char,string> parse_header(string line, int delimiter(int) = ::isspace ){
+map<char,string> parse_header(string line, int delimiter(int) = ::isspace )
+{
 
     string token;
 
@@ -62,7 +63,89 @@ map<char,string> parse_header(string line, int delimiter(int) = ::isspace ){
 
     return result;
 }
+void frame_decoding420(ifstream const & file, int const & end, int loop, int yCols, int yRows)
+{
 
+    int & end_t = const_cast<int &>(end);
+    /* Opening video file */
+    ifstream & myfile = const_cast<ifstream &>(file);
+
+    /* auxiliary variables */
+    int x, y, r, g, b, y_, u, v;
+
+    string line;
+
+    /* file data buffer */
+    unsigned char *imgData;
+    /* unsigned char pointer to the Mat data*/
+    uchar *buffer;
+
+
+    while(1){
+        /* data structure for the OpenCv image */
+        Mat img = Mat(Size(yCols, yRows), CV_8UC3);
+
+        /* buffer to store the frame */
+        imgData = new unsigned char[yCols * yRows * 3];
+
+        getline (myfile,line); // Skipping word FRAME
+        myfile.read((char *)imgData, yCols * yRows * 3);
+        if(myfile.gcount() == 0)
+        {
+            if(loop)
+            {
+                myfile.clear();
+                myfile.seekg(0);
+                getline (myfile,line); // read the header continue;
+            }
+            else
+            {
+                end_t= 1;
+                break;
+            }
+        }
+
+        /* The video is stored in YUV planar mode but OpenCv uses packed modes*/
+        buffer = (uchar*)img.ptr();
+
+        auto size = yRows * yCols;
+        auto width = yCols;
+        for(y = 0 ; y < yRows; y += 1)
+        {
+            for(x = 0 ; x < yCols; x += 1){
+                /* Accessing to planar info */
+                y_ = imgData[ y * width + x ];
+                u = imgData[(int) ( (y/2) * (width/2) + (x/2) + size)];
+                v = imgData[(int) ( (y/2) * (width/2) + (x/2) + size + (size/4))];
+
+                /* convert to RGB */
+                // b = (int)(1.164*(y - 16) + 2.018*(u-128));
+                // g = (int)(1.164*(y - 16) - 0.813*(u-128) - 0.391*(v-128));
+                // r = (int)(1.164*(y - 16) + 1.596*(v-128));
+
+                int r = y_ + (1.370705 * (v-128));
+                int g = y_ - (0.698001 * (v-128)) - (0.337633 * (u-128));
+                int b = y_ + (1.732446 * (u-128));
+
+                /* clipping to [0 ... 255] */
+                if(r < 0) r = 0;
+                if(g < 0) g = 0;
+                if(b < 0) b = 0;
+                if(r > 255) r = 255;
+                if(g > 255) g = 255;
+                if(b > 255) b = 255;
+
+                /* Fill the OpenCV buffer - packed mode: BGRBGR...BGR */
+                buffer[ y*3*width + 3* x ] = b;
+                buffer[(y*3*width + 3*x) + 1] = g;
+                buffer[(y*3*width + 3*x) + 2] = r;
+                //cout << "x: "<< x << " y: "<<y<<endl;
+            }
+        }
+
+        q.enqueue(img);
+    }
+}
 void frame_decoding444(ifstream const & file, int const & end, int loop, int yCols, int yRows) {
 
     int & end_t = const_cast<int &>(end);
@@ -182,8 +265,8 @@ void frame_decoding422(ifstream const & file, int const & end, int loop, int yCo
         for(i = 0 ; i < yRows * yCols * 3 ; i += 3)
         {
             /* Accessing to planar info */
-            y = imgData[i / 3]; 
-            u = imgData[(i / 6) + (yRows * yCols)]; 
+            y = imgData[i / 3];
+            u = imgData[(i / 6) + (yRows * yCols)];
             v = imgData[(i / 6) + (yRows * yCols) + ((yRows * yCols)/2)]; 
 
 
@@ -280,14 +363,19 @@ int main(int argc, char** argv)
     getline(myfile,line);
     map<char,string> header = parse_header(line);
 
+
     yCols = stoi(header['W']);
     yRows = stoi(header['H']);
 
     std::thread t;
-    if(stoi(header['C']) == 444){
-        t = std::thread(frame_decoding444, ref(myfile), ref(end), loop, yCols, yRows);
-    }else if(stoi(header['C']) == 422){
-        t = std::thread(frame_decoding422, ref(myfile), ref(end), loop, yCols, yRows);
+    if( header.count('C') != 0){
+        if(stoi(header['C']) == 444){
+            t = std::thread(frame_decoding444, ref(myfile), ref(end), loop, yCols, yRows);
+        }else if(stoi(header['C']) == 422){
+            t = std::thread(frame_decoding422, ref(myfile), ref(end), loop, yCols, yRows);
+        }
+    }else {
+        t = std::thread(frame_decoding420, ref(myfile), ref(end), loop, yCols, yRows);
     }
 
     /* create a window */
